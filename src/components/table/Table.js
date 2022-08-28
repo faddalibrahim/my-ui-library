534d70449ae855dd550d110deb5e577f228486e0
Faddal Ibrahim <faddalibrahim@gmail.com>
2022-08-28 17:00:50 +0000

    add limit to table

---
+++ src/components/table/Table.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+import tableStyles from "./table.module.css";
+
+const Table = ({ caption, headers, body, style, shadow, limit = 3 }) => {
+  const switchPage = (pageNumber) => {
+    let end = Number(pageNumber) * limit;
+    let start = end - limit;
+
+    setCurrentRows(body.slice(start, end));
+  };
+
+  const [currentRows, setCurrentRows] = useState([...body.slice(0, limit)]);
+
+  let pages = Array(Math.ceil(body.length / limit))
+    .fill()
+    .map((v, i) => <small onClick={() => switchPage(i)}>{++i}</small>);
+
+  return (
+    <div className={tableStyles.tableWrapper} style={{ ...style }}>
+      <table cellSpacing="0">
+        <caption>
+          <h1>{caption}</h1>
+        </caption>
+        <thead>
+          <tr>
+            {headers?.map((headName, index) => {
+              return <th key={index}>{headName}</th>;
+            }) ?? <div>loading...</div>}
+          </tr>
+        </thead>
+        <tbody>
+          {currentRows?.map((data, index) => (
+            <tr key={index}>
+              {Object.keys(data).map((key, index) => (
+                <td key={index}>{data[key]}</td>
+              ))}
+            </tr>
+          )) ?? <div>loading...</div>}
+        </tbody>
+      </table>
+      <div className={tableStyles.pages}>
+        <center>{pages}</center>
+      </div>
+    </div>
+  );
+};
+
+Table.propTypes = {
+  /** Title of the table */
+  caption: PropTypes.string,
+
+  /**table headers */
+  headers: PropTypes.arrayOf(PropTypes.string),
+
+  /**all rows in the table */
+  body: PropTypes.arrayOf(PropTypes.string),
+
+  /**custom additional styling */
+  style: PropTypes.objectOf(PropTypes.string),
+
+  /**apply shadow around the table border */
+  shadow: PropTypes.bool,
+
+  /**the number of rows per page */
+  limit: PropTypes.number,
+};
+
+export default Table;
---
+++ src/stories/Table.stories.js
@@ -0,0 +1,27 @@
+import Table from "../components/table/Table";
+
+export default {
+  title: "Table",
+  component: Table,
+};
+
+const Template = (args) => <Table {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  caption: "Table",
+  headers: ["name", "school", "year"],
+  body: [
+    { name: "Dolores Abernathy", school: "Ashesi University", year: "2027" },
+    { name: "Angela Torsu", school: "University of Ghana", year: "2020" },
+    { name: "Dolores Abernathy", school: "Academic City", year: "2022" },
+    { name: "Michael Jordan", school: "Winsconsin University", year: "2025" },
+    { name: "Farida Sheini", school: "Columbia University", year: "2030" },
+    { name: "Faddal Ibrahim", school: "Columbia University", year: "2050" },
+    { name: "Alhassan Hassan", school: "Meta University", year: "2040" },
+    { name: "Fawzan Abdallah", school: "Apple University", year: "2035" },
+  ],
+  style: null,
+  shadow: true,
+};
